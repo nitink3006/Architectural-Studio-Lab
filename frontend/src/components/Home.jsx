199c6341e944874a750760e15137203dc62e63e7
Utpanna Pradhan <Utpanna828@gmail.com>
2025-02-17 22:46:52 +0530

    test

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-extrabold text-blue-600">Hello World</h1>
+      <h1 className="text-4xl font-extrabold text-blue-600">Hello---------</h1>
     </div>
   );
 };
