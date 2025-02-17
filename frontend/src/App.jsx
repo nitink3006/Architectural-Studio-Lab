ac9c1e26603af52f45b906ac6d46e32ec38e270e
Nitin Kumar <nitink710btechaiml2025@kccitm.edu.in>
2025-02-17 22:06:21 +0530

    setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home";
+
+function App() {
+  return (
+    <Router>
+      {/* Set up Routes */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
