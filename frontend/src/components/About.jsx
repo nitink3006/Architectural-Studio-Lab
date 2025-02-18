848224c8ab4bec372719d556dd1e125b761bd701
ErNikhil0 <nikhiln249btechaiml2025@kccitm.edu.in>
2025-02-18 17:42:43 +0530

    update about

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -10,12 +10,9 @@ const About = () => {
   return (
     <div className="relative w-full h-[200vh] text-black flex flex-col">
       <Navigation />
-      {/* About Us Section (30% of viewport) */}
       <div className="w-full h-[45vh] flex items-center justify-center">
         <h1 className="text-4xl md:text-7xl font-bold">About Us</h1>
       </div>
-
-      {/* Full-size image with scroll animation */}
       <div className="relative w-full h-[100vh] sticky top-0 overflow-hidden flex justify-center items-center">
         <motion.img
           src="/aboutmain1.jpg"
@@ -25,7 +22,6 @@ const About = () => {
         />
       </div>
       
-      {/* Scrolling text animation */}
       <div className="relative w-full h-[50vh] flex items-center justify-center">
         <motion.div
           className="text-2xl md:text-4xl font-bold text-white bg-opacity-40 p-6 rounded-lg w-[50%] h-[40%] flex items-center justify-center"
