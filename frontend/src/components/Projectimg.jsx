66f358badb191c175cf226672c6d2d582335aa06
Utpanna Pradhan <Utpanna828@gmail.com>
2025-04-14 00:27:17 +0530

    image update

--- frontend/src/components/Projectimg.jsx
+++ frontend/src/components/Projectimg.jsx
@@ -2,57 +2,49 @@ import React, { useRef, useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 import "../Stylesheet/Project.css";
+import hos4 from "../assets/home/hospitality/12we3.JPG";
+import hos5 from "../assets/home/hospitality/270225120_369220955007362_8042417773141522535_n.jpg";
+import hos6 from "../assets/home/hospitality/270294406_407373781087132_3783180291927619870_n.jpg";
+import com1 from "../assets/home/commercial/1 - Workstation Area_Revised.jpg";
+import com2 from "../assets/home/commercial/01 copy.jpg";
+import com3 from "../assets/home/commercial/1.JPG";
+
+import com7 from "../assets/home/commercial/8 - Conference Room.jpg";
+import com8 from "../assets/home/commercial/9 - Cabin_Revised.jpg";
+import com9 from "../assets/home/commercial/9.JPG";
+
+import com13 from "../assets/home/commercial/IMG-20240913-WA0042.jpg";
+import com14 from "../assets/home/commercial/Lobby-B-02.jpg";
+
+
+import res11 from "../assets/home/residential/Screenshot 2024-11-27 235321.png";
+import res12 from "../assets/home/residential/Screenshot 2024-11-27 235451.png";
+import res13 from "../assets/home/residential/Screenshot 2024-11-27 235519.png";
+import rec4 from "../assets/home/recreational/Reception View-01.jpg";
+
 
 const projects = [
+  //--------------------------row1----------------------------------------------------
   {
-    src: "https://static.wixstatic.com/media/820b23_7f3179837f104ca690816660998f87a5~mv2.jpg/v1/fill/w_464,h_300,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/Copy%20of%20DSC08585%20copy%20(3).jpg",
+    src: com1,
     title: "The Japandi Home",
     location: "Pioneer Araya",
-    category: "Residential",
+    category: "Commercial",
     size: "3500 sqft",
     place: " Gurugram",
     link: "/Demo2",
   },
   {
-    src: "https://static.wixstatic.com/media/820b23_aa3ad3e8319e40338fe5be92d1357647~mv2.jpg/v1/fill/w_464,h_300,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/Copy%20of%20YJP-CS-20.jpg",
+    src: com2,
     title: "The Zephyr Villa",
     location: "Goa Villa",
-    category: "Hospitality",
+    category: "Commercial",
     size: "3000 sqft",
     place: "Siolim , Goa",
     link: "/Projectcontent1",
   },
   {
-    src: "https://static.wixstatic.com/media/820b23_54b09147c2604fef86e847a052c098c9~mv2.png/v1/fill/w_464,h_300,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/Screenshot%202024-05-01%20at%2011_04_24%20AM.png",
-    title: "Home in the Hills",
-    location: "Rishikesh Villa",
-    category: "Hospitality",
-    size: "5000 sqft",
-    place: "Rishikesh",
-    link: "/Projectcontent1",
-  },
-
-  //------------------------------------------ row2-----------------------------------------------
-  {
-    src: "https://static.wixstatic.com/media/820b23_fb3439180db948b8b6e2ce10727307c1~mv2.png/v1/fill/w_464,h_300,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/Living_AA.png",
-    title: "The Japandi Home",
-    location: "Pioneer Araya",
-    category: "Residential",
-    size: "3500 sqft",
-    place: " Gurugram",
-    link: "/Projectcontent1",
-  },
-  {
-    src: "https://static.wixstatic.com/media/748419_fd9994ead9634ad188c56d1c4fc444e2~mv2.jpg/v1/fill/w_464,h_300,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/E_PHX_2822LR.jpg",
-    title: "The Zephyr Villa",
-    location: "Goa Villa",
-    category: "Hospitality",
-    size: "3000 sqft",
-    place: "Siolim , Goa",
-    link: "/Projectcontent1",
-  },
-  {
-    src: "https://static.wixstatic.com/media/76aad7_70bd94d3a60b4c7eb4aa98db1433fa5b~mv2.jpg/v1/fill/w_464,h_300,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/%C2%A9AG-DSC03040-Edit-Edit-min.jpg",
+    src: com3,
     title: "Home in the Hills",
     location: "Rishikesh Villa",
     category: "Commercial",
@@ -60,10 +52,61 @@ const projects = [
     place: "Rishikesh",
     link: "/Projectcontent1",
   },
+ //------------------------------------------ row12(residential)-----------------------------------------------
+   {
+    src: res11,
+    title: "The Japandi Home",
+    location: "Pioneer Araya",
+    category: "Residential",
+    size: "3500 sqft",
+    place: " Gurugram",
+  },
+  {
+    src: res12,
+    title: "The Zephyr Villa",
+    location: "Goa Villa",
+    category: "Residential",
+    size: "3000 sqft",
+    place: "Siolim , Goa",
+  },
+
+  {
+    src: res13,
+    title: "Home in the Hills",
+    location: "Rishikesh Villa",
+    category: "Residential",
+    size: "5000 sqft",
+    place: "Rishikesh",
+  },
+   //------------------------------------------ row7 (hospitality)-----------------------------------------------
+        {
+          src: hos4,
+          title: "The Japandi Home",
+          location: "Pioneer Araya",
+          category: "Hospitality",
+          size: "3500 sqft",
+          place: " Gurugram",
+        },
+        {
+          src: hos5,
+          title: "The Zephyr Villa",
+          location: "Goa Villa",
+          category: "Hospitality",
+          size: "3000 sqft",
+          place: "Siolim , Goa",
+        },
+        {
+          src: hos6,
+          title: "Home in the Hills",
+          location: "Rishikesh Villa",
+          category: "Hospitality",
+          size: "5000 sqft",
+          place: "Rishikesh",
+        },
 
   //------------------------------------------ row3-----------------------------------------------
   {
-    src: "https://static.wixstatic.com/media/748419_c30fb606858b433ca25092a1c00c0e7e~mv2.jpg/v1/fill/w_464,h_300,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/ChalkDS_Mang_-2-min.jpg",
+    src: com7,
     title: "The Japandi Home",
     location: "Pioneer Araya",
     category: "Commercial",
@@ -71,47 +114,53 @@ const projects = [
     place: " Gurugram",
   },
   {
-    src: "https://static.wixstatic.com/media/055c2c_e070114d9c5a49bd9e1de8e8e274fce9~mv2.jpg/v1/fill/w_464,h_300,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/CS-PortoHome_5_edited_edited.jpg",
+    src: com8,
     title: "The Zephyr Villa",
     location: "Goa Villa",
-    category: "Hospitality",
+    category: "Commercial",
     size: "3000 sqft",
     place: "Siolim , Goa",
   },
   {
-    src: "https://static.wixstatic.com/media/76aad7_f647086adfb54b11a8d267054d97a761~mv2.jpg/v1/fill/w_464,h_300,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/_SND7411.jpg",
+    src: com9,
     title: "Home in the Hills",
     location: "Rishikesh Villa",
-    category: "Hospitality",
+    category: "Commercial",
     size: "5000 sqft",
     place: "Rishikesh",
   },
 
-  //------------------------------------------ row4-----------------------------------------------
+  
+
+
+  //------------------------------------------ row5-----------------------------------------------
   {
-    src: "https://static.wixstatic.com/media/aaeed6_328be93fa6b54a79b22163bf3bd2f276~mv2.jpg/v1/fill/w_464,h_300,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/%C2%A9AG-CS-DSC00787-HDR%20copy%202%20(1).jpg",
+    src: com13,
     title: "The Japandi Home",
     location: "Pioneer Araya",
-    category: "Residential",
+    category: "Commercial",
     size: "3500 sqft",
     place: " Gurugram",
   },
   {
-    src: "https://static.wixstatic.com/media/820b23_862ca02c4d884d18adce9c3bcc7e3621~mv2.png/v1/fill/w_464,h_300,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/Copy%20of%20Living%20(16-02-2022).png",
+    src: com14,
     title: "The Zephyr Villa",
     location: "Goa Villa",
     category: "Commercial",
     size: "3000 sqft",
     place: "Siolim , Goa",
   },
+
   {
-    src: "https://static.wixstatic.com/media/25240c_cee6dd7418e54f75acd64f1f4b1d5a9c~mv2.jpg/v1/fill/w_464,h_300,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/01%20(10).jpg",
-    title: "Home in the Hills",
-    location: "Rishikesh Villa",
-    category: "Hospitality",
-    size: "5000 sqft",
-    place: "Rishikesh",
-  },
+      src: rec4,
+      title: "Home in the Hills",
+      location: "Rishikesh Villa",
+      category: "Recreational",
+      size: "5000 sqft",
+      place: "Rishikesh",
+    },
+ 
+  
 ];
 
 const Projectimg = ({ selectedCategory }) => {
@@ -157,6 +206,7 @@ const Projectimg = ({ selectedCategory }) => {
 
   return (
     <div className="mt-2 pb-15 proj_home">
+  
       {/* bg-Soft_Grey */}
       {groupedProjects.length === 0 ? (
         <p className="no-results">No projects found in this category.</p>
